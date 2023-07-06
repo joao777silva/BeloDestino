72810c0d106955158e7a3d92d79d0238aee82711
joao777silva <joaosilva.lilzinn15@gmail.com>
2023-07-05 21:48:19 -0300

    update belo Destino rev.5

--- js/menu.js
+++ js/menu.js
@@ -14,3 +14,28 @@ function menu(){
 
 
 }
+
+/* arquivo das automaçoes do menu hamburger*/
+const btnMobile = document.getElementById ('btn-mobile');
+// declarando a variavel com CONST
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') Event.preventDefault ();
+    const nav = document.getElementById('nav');    
+    nav.classList.toggle ('active');
+    const  active = nav.classList.contains(active);
+    event.currentTarget.setAttribute('aria-Expanded', active);
+        if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+    }   else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+
+    
+}
+
+
+    
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
